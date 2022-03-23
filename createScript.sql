48e577e28295dbd161bc3023d4b5f7e94bb0d5e2
smethven <smethven@student.ubc.ca>
2022-03-23 11:26:57 -0700

    added work from Charlie's branch, fixed quote syntax, Order not working on sqlplus

--- createScript.sql
+++ createScript.sql
@@ -6,35 +6,43 @@ CREATE TABLE Customer(
     PRIMARY KEY(account_username)
 );
 
+CREATE TABLE FoodProvider(
+    food_provider_name varchar(50),
+    food_provider_location varchar(50),
+    phone_number char(10),
+    PRIMARY KEY(food_provider_name, food_provider_location)
+);
+
+CREATE TABLE Order(
+    order_number char(7),
+	order_price float(4),
+	order_time datetime,
+	account_username varchar(20) NOT NULL,
+	food_provider_name varchar(50) NOT NULL,
+	food_provider_location varchar(50) NOT NULL,
+	PRIMARY KEY(order_number),
+	FOREIGN KEY(account_username) REFERENCES Customer ON DELETE CASCADE,
+    FOREIGN KEY(food_provider_name) REFERENCES FoodProvider ON DELETE CASCADE,
+	FOREIGN KEY(food_provider_location) REFERENCES FoodProvider ON DELETE CASCADE
+);
 
 INSERT INTO Customer VALUES ('Ellen123', 'ellen123@gmail.com','2366 Main Mall', 'Ellen');
 INSERT INTO Customer VALUES ('Skye321', 'Skye321@gmail.com','6245 Agronomy Rd','Skye');
 INSERT INTO Customer VALUES ('Charlie5', 'Charlie5@gmail.com','2335 Engineering Rd', 'Charlie');
 INSERT INTO Customer VALUES ('RaymondGod', 'RaymondGod304@gmail.com','2036 Main Mall', 'Raymond');
 INSERT INTO Customer VALUES ('MohammedLee', 'theMostCommonName@gmail.com','2366 Main Mall', 'MohammedLee');
-CREATE TABLE Order(
-    order_number  char(25),
-	order_price double,
-	order_time datetime,     
-	account_username varchar(20) NOT NULL,
-	food_provider_name varchar(50) NOT NULL,
-	food_provider_location varchar(50) NOT NULL,
-	PRIMARY KEY(order_number),
-	FOREIGN KEY(account_username) REFERENCES Customer ON DELETE CASCADE
-    FOREIGN KEY(food_provider_name) REFERENCES FoodProvider ON DELETE CASCADE
-	FOREIGN KEY(food_provider_location) REFERENCES FoodProvider ON DELETE CASCADE
-)
 
-INSERT INTO Order
-VALUES(’1000000’, 39.5, ’2022-03-03 13:23:44’,’Ellen123’, ‘McDonald’s’, ‘5728 University Blvd #101, Vancouver’ )
-INSERT INTO Order
-VALUES(‘1000001’, 40.01, ’2022-03-03 20:23:12’,’Skye321’, ‘McDonald’s’, ‘5728 University Blvd #101, Vancouver’ )
-INSERT INTO Order
-VALUES(’1000002’, 27.15, ’2022-02-28 21:03:09’, ‘Charlie5’, ‘McDonald’s’, ‘5728 University Blvd #101, Vancouver’ )
-INSERT INTO Order
-VALUES(1000003, 70.5, ’2022-03-03 13:23:44’, ’RaymondGod’, ‘Pho 37’, ‘8328 Capstan Way #1101, Richmond’ )
-INSERT INTO Order
-VALUES(’1000004’, 20.08, ‘2022-03-03 20:13:00’, ‘Ellen123’, ‘McDonald’s’, ‘5728 University Blvd #101, Vancouver’ )
+INSERT INTO FoodProvider VALUES ('McDonalds', '5728 University Blvd #101, Vancouver','6042212570');
+INSERT INTO FoodProvider VALUES ('The Keg Steakhouse + Bar', '688 Dunsmuir St., Vancouver','6046857502');
+INSERT INTO FoodProvider VALUES ('The Home Depot', '3950 Henning Dr, Burnaby', '6042943030');
+INSERT INTO FoodProvider VALUES ('Pho 37','8328 Capstan Way #1101, Richmond','6044475158');
+INSERT INTO FoodProvider VALUES ('Cactus Club Richmond Centre','6511 No.3 Rd #1666, Richmond','6042449969');
+
+INSERT INTO Order VALUES('1000000', 39.50, '2022-03-03 13:23:44','Ellen123', 'McDonalds', '5728 University Blvd #101, Vancouver');
+INSERT INTO Order VALUES('1000001', 40.01, '2022-03-03 20:23:12','Skye321', 'McDonalds', '5728 University Blvd #101, Vancouver');
+INSERT INTO Order VALUES('1000002', 27.15, '2022-02-28 21:03:09', 'Charlie5', 'McDonalds', '5728 University Blvd #101, Vancouver');
+INSERT INTO Order VALUES('1000003', 70.50, '2022-03-03 13:23:44', 'RaymondGod', 'Pho 37', '8328 Capstan Way #1101, Richmond');
+INSERT INTO Order VALUES('1000004', 20.08, '2022-03-03 20:13:00', 'Ellen123', 'McDonalds', '5728 University Blvd #101, Vancouver');
 
 
 
--- dropScript.sql
+++ dropScript.sql
@@ -1,3 +1,3 @@
 DROP TABLE Customer;
-
+DROP TABLE FoodProvider;
 DROP TABLE Order;
